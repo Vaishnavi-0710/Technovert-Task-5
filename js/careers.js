7e5aea81bb7bcd9b992a15dfabce728a4a2bc9df
Vaishnavi Dhayapule <dhayapulevaishnavi07@gmail.com>
2022-11-10 19:24:45 +0530

    commit

--- js/careers.js
+++ js/careers.js
@@ -1,44 +1,46 @@
 function validateName(){
-    let name=document.getElementById('name').value;
+    let name=$("#name").val();
     if(name!=""){
-        document.getElementById('nameError').innerHTML="";
+        $('#nameError').text("");
         return true;
     }
     else{
-        document.getElementById('nameError').innerHTML="Name is required";
-        return(false);
-    }
-}
-  
-function validateEmail(){
-    let email=document.getElementById("email").value;
-    const validMail=/^[a-zA-Z0-9.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9.]{2,}$/;
-    if(email.match(validMail) || email==""){
-        if(email==""){
-        document.getElementById('emailError').innerHTML="Email is required";
+        $('#nameError').text("Name is required");
         return false;
     }
+};
+  
+function validateEmail(){
+    let email=$("#email").val();
+    const mail=/^[a-zA-Z0-9.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9.]{2,}$/;
+    if(mail.test(email) || email==""){
+        if(email==""){
+            $('#emailError').text("Email is required");
+            return false;
+        }
         else{
-        document.getElementById("emailError").innerHTML="";
-        return true;
+            $('#emailError').text("");
+            return true;
         }
     }
     else{
-        document.getElementById("emailError").innerHTML="Enter valid Email";
+        $("#emailError").text("Enter Valid Email");
         return false;
     }
 }
-
-document.getElementById("submit").addEventListener("click", (e)=>{
-    e.preventDefault();
-    let name=validateName();
-    let email=validateEmail();
-    if (name && email){
-        alert("Successfully submitted");
-        document.getElementById("details").reset();
-        document.getElementById("update").innerHTML="";
-    }
-    else{
-        document.getElementById("update").innerHTML="Please fill all the required fileds below";
-    }
+  
+$(document).ready(function(){
+    $("#submit").click(function(e){
+        e.preventDefault();
+        let name=validateName();
+        let email=validateEmail();
+        if (name && email){
+            alert("Successfully submitted");
+            $("#details").trigger("reset");
+            $("#update").text("");
+        }
+        else{
+            $("#update").text("Please fill all the required fields below");
+        }
+    });
 });
